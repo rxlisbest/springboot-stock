34a4f259bdfd52e5daa59b439374f95e061a56dd
Roy <rxlisbest@gmail.com>
2019-02-25 09:22:01 +0800

    @Data

--- src/main/java/com/ruiruisun/stock/entity/User.java
+++ src/main/java/com/ruiruisun/stock/entity/User.java
@@ -1,45 +1,13 @@
 package com.ruiruisun.stock.entity;
 
-/**
- *
- */
+import lombok.Data;
+
+@Data
 public class User {
-    private Integer id;
+    private int id;
     private String userName;
     private String passWord;
     private String realName;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassWord() {
-        return passWord;
-    }
-
-    public void setPassWord(String passWord) {
-        this.passWord = passWord;
-    }
-
-    public String getRealName() {
-        return realName;
-    }
-
-    public void setRealName(String realName) {
-        this.realName = realName;
-    }
 
     @Override
     public String toString() {
