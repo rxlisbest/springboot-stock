8f32fcf164d39d04e8d3f4860eae9d10850adf61
Roy <rxlisbest@gmail.com>
2019-03-10 12:58:32 +0800

    jar -> war

---
+++ README.md
@@ -0,0 +1,13 @@
+# 前端项目
+https://github.com/rxlisbest/vue-stock.git
+
+# 项目
+### 框架
+- springboot
+- mybatis
+
+### 接口风格
+- restful
+
+### 权限
+- jwt
--- src/main/java/com/ruiruisun/stock/StockApplication.java
+++ src/main/java/com/ruiruisun/stock/StockApplication.java
@@ -3,15 +3,19 @@ package com.ruiruisun.stock;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @MapperScan("com.ruiruisun.stock.mapper")
 @SpringBootApplication
-public class StockApplication {
-
+public class StockApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(StockApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(StockApplication.class, args);
     }
-
 }
 
