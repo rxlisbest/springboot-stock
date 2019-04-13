625b07546e439466aca2e0b0cf679e8e4d505175
Roy <rxlisbest@gmail.com>
2019-04-13 21:09:50 +0800

    增加用户权限

---
+++ src/main/resources/db/migration/V11__insertRoleApi.sql
@@ -0,0 +1,12 @@
+use `stock`;
+
+-- goods_categories
+INSERT INTO `role_api` (`role_id`, `api_id`, `create_time`, `update_time`) VALUES (2, 8, unix_timestamp(now()), 0);
+INSERT INTO `role_api` (`role_id`, `api_id`, `create_time`, `update_time`) VALUES (2, 9, unix_timestamp(now()), 0);
+INSERT INTO `role_api` (`role_id`, `api_id`, `create_time`, `update_time`) VALUES (2, 14, unix_timestamp(now()), 0);
+-- goods
+INSERT INTO `role_api` (`role_id`, `api_id`, `create_time`, `update_time`) VALUES (2, 15, unix_timestamp(now()), 0);
+INSERT INTO `role_api` (`role_id`, `api_id`, `create_time`, `update_time`) VALUES (2, 16, unix_timestamp(now()), 0);
+INSERT INTO `role_api` (`role_id`, `api_id`, `create_time`, `update_time`) VALUES (2, 21, unix_timestamp(now()), 0);
+-- goods_logs
+INSERT INTO `role_api` (`role_id`, `api_id`, `create_time`, `update_time`) VALUES (2, 22, unix_timestamp(now()), 0);
